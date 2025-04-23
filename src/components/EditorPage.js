3ef0dc68b9ee617bc4e371853c7f54198a279383
shakaib <shakaib.ahmad@cam-systems.co.uk>
2025-04-23 14:26:10 +0500

    dependency

--- src/components/EditorPage.js
+++ src/components/EditorPage.js
@@ -68,7 +68,7 @@ export default function EditorPage({ images: incomingImages }) {
                 }
             }
         });
-    }, [slots]);
+    }, [slots, pageSettings]);
 
     // persist
     useEffect(() => {
