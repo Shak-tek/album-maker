674842126d813bb8ed1131d8475041e5b5c07503
Shak-tek <shakaib.tekfirst@gmail.com>
2025-07-03 21:15:28 +0200

    Add CORS support

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const ImageKit = require('imagekit');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
+const corsOptions = { origin: process.env.CORS_ORIGIN || 'http://localhost:3000' };
+app.use(cors(corsOptions));
 
 const imagekit = new ImageKit({
   publicKey: process.env.IMAGEKIT_PUBLIC_KEY || '',
