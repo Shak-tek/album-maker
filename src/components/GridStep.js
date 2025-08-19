c3755120d9cdfc4099f888273b4c878f60069e54
Awais <Muhammadawaisabbas@gmail.com>
2025-08-19 15:13:31 +0500

    ui

--- src/components/GridStep.js
+++ src/components/GridStep.js
@@ -20,7 +20,7 @@ export default function GridStep({
 }) {
     return (
         <Box>
-            <Grid className="uploader-grid" rows="small" columns={["small", "small", "small", "small", "small", "small"]} gap="small">
+            <Grid className="uploader-grid" rows="small" columns={["small", "small", "small", "small", "small", "small"]} gap="s4">
                 {uploads.length < 24 && (
                     <Box
                         key="add"
