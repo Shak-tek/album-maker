dcc4b63704569628ed4a9b0dbc40b85888a16f27
Awais <Muhammadawaisabbas@gmail.com>
2025-08-22 17:21:14 +0500

    ui

--- src/components/TemplateModal.js
+++ src/components/TemplateModal.js
@@ -15,7 +15,7 @@ export default function TemplateModal({ onSelect, onClose }) {
             <Box pad="medium" className="modalTemplates" width="" gap="medium" style={{ maxWidth: '90vw' }}>
                 <Box direction="row" justify="between" align="center">
                     <Text weight="bold">Templates</Text>
-                    <Button label="Close" className="modalClose" Text="white" onClick={onClose} />
+                    <Button label="X" className="modalClose" Text="white" onClick={onClose} />
                 </Box>
 
                 <Box className="btnLayoutWrap" wrap direction="row" gap="small">
